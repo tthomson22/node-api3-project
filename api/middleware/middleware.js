0587c71cd79997d3a329c4d0ef99c6bde8d83d78
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-13 15:06:56 -0700

    logger set up

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,11 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const method = req.method
+  const url = req.originalUrl
+  const timeStamp = new Date().toLocaleString()
+  console.log(`${method}, ${url}, ${timeStamp}`)
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +21,6 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,9 @@ const express = require('express');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -42,3 +45,4 @@ router.post('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router
